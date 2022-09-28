74277da7716ab1b9e86d1715b3ad13cb244367de
EU6KS1emA <bemmda468@gmail.com>
2022-09-28 10:48:46 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     let nfts; 
     console.log("fetching nfts");
     
-    const api_key = ""
+    const api_key = "tDs9rsZL5spHQKJ93lDZShRyTcGSI3nW"
 
 
 
@@ -44,7 +44,7 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = ""
+      const api_key = "tDs9rsZL5spHQKJ93lDZShRyTcGSI3nW"
 
 
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
